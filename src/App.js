dcbccf0f38742d438763b8908b8c580d77c766ad
N-I-H-A-L <notesnihalengg@gmail.com>
2023-04-03 12:54:56 +0530

    Setting up the File System

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+import { Switch } from "react-router";
+import { Layout, Typography, Space } from 'antd';
 
-function App() {
+import './App.css'
+
+import {Navbar, Homepage, Exchanges, Cryptocurrencies, News, CryptoDetails} from './components';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='app'>
+      <div className="navbar">
+        <Navbar/>
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+{/* exact keyword says it will open the component inside it if and only if the path is exactly equal to '/' */}
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
 
-export default App;
+              {/* Colon specifies that the address will be dynamic, e.g. /crypto/123, /crypto/xyz, etc. */}
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes> 
+          </div>
+        </Layout>
+
+        <div className='footer'>
+          <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}> 
+            Cryptoverse <br/>
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  )
+}
