6d2dedc5f5e0201c316ccbb875c32ae9db0f67a4
N-I-H-A-L <notesnihalengg@gmail.com>
2023-04-05 17:31:13 +0530

    News Component

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import {cryptoApi} from '../services/cryptoAPI';
+import { cryptoNewsApi } from "../services/cryptoNewsApi";
 
 export default configureStore({
     reducer: {
         [cryptoApi.reducerPath] : cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath] : cryptoNewsApi.reducer,
     },
     //Adding the API in the middleware
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(cryptoApi.middleware),
+        getDefaultMiddleware().concat([cryptoApi.middleware, cryptoNewsApi.middleware]),
 });
